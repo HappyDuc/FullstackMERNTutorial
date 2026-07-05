1cf4a38e867b8060ea5acee46c987cb109d363ae
Jake Sinclair <sinclairs1234@gmail.com>
2026-07-05 10:07:03 +0100

    first commit

---
+++ src/components/DisplayImage.jsx
@@ -0,0 +1,18 @@
+import { Image } from "@imagekit/react";
+
+const DisplayImage = ({ src, className, w, h, alt }) => {
+    return (
+        <Image
+            urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT}
+            src={src}
+            className={className}
+            loading="lazy"
+            lqip={{ active: true, quality: 20 }}
+            alt={alt}
+            width={w}
+            height={h}
+        />
+    );
+};
+
+export default DisplayImage;
