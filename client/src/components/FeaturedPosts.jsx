69722d90b46689fb992b226990633a13b179c7d6
Jake Sinclair <sinclairs1234@gmail.com>
2026-07-20 23:09:12 +0100

    Finalised ability to sort page list and implemented post featuring.

--- client/src/components/FeaturedPosts.jsx
+++ client/src/components/FeaturedPosts.jsx
@@ -1,120 +1,165 @@
 import { Link } from "react-router";
 import DisplayImage from "./DisplayImage";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { format } from "timeago.js";
+
+const fetchPost = async () => {
+    const res = await axios.get(
+        `${import.meta.env.VITE_API_URL}/posts?featured=true&limit=4&sort=newest`,
+    );
+    return res.data;
+};
 
 const FeaturedPosts = () => {
+    const { isPending, error, data } = useQuery({
+        queryKey: ["featuredPosts"],
+        queryFn: () => fetchPost(),
+    });
+
+    if (isPending) return "Loading...";
+
+    if (error) return "Something went wrong..." + error.message;
+
+    const posts = data.posts;
+    if (!posts || posts.length === 0) {
+        return;
+    }
+
     return (
         <div className="mt-8 flex flex-col lg:flex-row gap-8">
             {/* First */}
             <div className="w-full lg:w-1/2 flex flex-col gap-4">
                 {/* Image */}
-                <DisplayImage
-                    src="featured1.jpeg"
-                    className="rounded-3xl object-cover"
-                    w="895"
-                />
+                {posts[0].img && (
+                    <DisplayImage
+                        src={posts[0].img}
+                        className="rounded-3xl object-cover"
+                        w="895"
+                    />
+                )}
                 {/* Details */}
                 <div className="flex items-center gap-4">
                     <h1 className="font-semibold lg:text-lg">01.</h1>
-                    <Link className="text-blue-800 lg:text-lg">Web Design</Link>
-                    <span className="text-gray-500">2 days ago</span>
+                    <Link className="text-blue-800 lg:text-lg">
+                        {posts[0].category}
+                    </Link>
+                    <span className="text-gray-500">
+                        {format(posts[0].createdAt)}
+                    </span>
                 </div>
                 {/* Title */}
                 <Link
-                    to="/test"
+                    to={posts[0].slug}
                     className="text-xl lg:text-3xl font-semibold lg:font-bold"
                 >
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    {posts[0].title}
                 </Link>
             </div>
             {/* Others */}
             <div className="w-full lg:w-1/2 flex flex-col gap-4">
                 {/* Second */}
-                <div className="lg:h-1/3 flex justify-between gap-4">
-                    <div className="w-1/3 aspect-video">
-                        <DisplayImage
-                            src="featured2.jpeg"
-                            className="rounded-3xl object-cover w-full h-full"
-                            w="298"
-                        />
-                    </div>
-                    {/* Details and title */}
-                    <div className="w-2/3">
-                        {/* Details */}
-                        <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
-                            <h1 className="font-semibold">02.</h1>
-                            <Link className="text-blue-500">Web Design</Link>
-                            <span className="text-gray-500 text-sm">
-                                2 days ago
-                            </span>
+                {posts[1] && (
+                    <div className="lg:h-1/3 flex justify-between gap-4">
+                        {posts[1].img && (
+                            <div className="w-1/3 aspect-video">
+                                <DisplayImage
+                                    src={posts[1].img}
+                                    className="rounded-3xl object-cover w-full h-full"
+                                    w="298"
+                                />
+                            </div>
+                        )}
+                        {/* Details and title */}
+                        <div className="w-2/3">
+                            {/* Details */}
+                            <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                                <h1 className="font-semibold">02.</h1>
+                                <Link className="text-blue-500">
+                                    {posts[1].category}
+                                </Link>
+                                <span className="text-gray-500 text-sm">
+                                    {format(posts[1].createdAt)}
+                                </span>
+                            </div>
+                            {/* Title */}
+                            <Link
+                                to={posts[1].slug}
+                                className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
+                            >
+                                {posts[1].title}
+                            </Link>
                         </div>
-                        {/* Title */}
-                        <Link
-                            to="/test"
-                            className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
-                        >
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit.
-                        </Link>
                     </div>
-                </div>
+                )}
                 {/* Third */}
-                <div className="lg:h-1/3 flex justify-between gap-4">
-                    <div className="w-1/3 aspect-video">
-                        <DisplayImage
-                            src="featured3.jpeg"
-                            className="rounded-3xl object-cover w-full h-full"
-                            w="298"
-                        />
-                    </div>
-                    {/* Details and title */}
-                    <div className="w-2/3">
-                        {/* Details */}
-                        <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
-                            <h1 className="font-semibold">03.</h1>
-                            <Link className="text-blue-500">Web Design</Link>
-                            <span className="text-gray-500 text-sm">
-                                3 days ago
-                            </span>
+                {posts[2] && (
+                    <div className="lg:h-1/3 flex justify-between gap-4">
+                        {posts[2].img && (
+                            <div className="w-1/3 aspect-video">
+                                <DisplayImage
+                                    src={posts[2].img}
+                                    className="rounded-3xl object-cover w-full h-full"
+                                    w="298"
+                                />
+                            </div>
+                        )}
+                        {/* Details and title */}
+                        <div className="w-2/3">
+                            {/* Details */}
+                            <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                                <h1 className="font-semibold">02.</h1>
+                                <Link className="text-blue-500">
+                                    {posts[2].category}
+                                </Link>
+                                <span className="text-gray-500 text-sm">
+                                    {format(posts[2].createdAt)}
+                                </span>
+                            </div>
+                            {/* Title */}
+                            <Link
+                                to={posts[2].slug}
+                                className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
+                            >
+                                {posts[2].title}
+                            </Link>
                         </div>
-                        {/* Title */}
-                        <Link
-                            to="/test"
-                            className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
-                        >
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit.
-                        </Link>
                     </div>
-                </div>
+                )}
                 {/* Fourth */}
-                <div className="lg:h-1/3 flex justify-between gap-4">
-                    <div className="w-1/3 aspect-video">
-                        <DisplayImage
-                            src="featured4.jpeg"
-                            className="rounded-3xl object-cover w-full h-full"
-                            w="298"
-                        />
-                    </div>
-                    {/* Details and title */}
-                    <div className="w-2/3">
-                        {/* Details */}
-                        <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
-                            <h1 className="font-semibold">04.</h1>
-                            <Link className="text-blue-500">Web Design</Link>
-                            <span className="text-gray-500 text-sm">
-                                2 days ago
-                            </span>
+                {posts[3] && (
+                    <div className="lg:h-1/3 flex justify-between gap-4">
+                        {posts[3].img && (
+                            <div className="w-1/3 aspect-video">
+                                <DisplayImage
+                                    src={posts[3].img}
+                                    className="rounded-3xl object-cover w-full h-full"
+                                    w="298"
+                                />
+                            </div>
+                        )}
+                        {/* Details and title */}
+                        <div className="w-2/3">
+                            {/* Details */}
+                            <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                                <h1 className="font-semibold">02.</h1>
+                                <Link className="text-blue-500">
+                                    {posts[3].category}
+                                </Link>
+                                <span className="text-gray-500 text-sm">
+                                    {format(posts[3].createdAt)}
+                                </span>
+                            </div>
+                            {/* Title */}
+                            <Link
+                                to={posts[3].slug}
+                                className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
+                            >
+                                {posts[3].title}
+                            </Link>
                         </div>
-                        {/* Title */}
-                        <Link
-                            to="/test"
-                            className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium"
-                        >
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit.
-                        </Link>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     );
